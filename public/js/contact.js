51ff50bfe7d847a2e8c9cce3dcca373d9408250e
Megan Middleton <uxtechnologist@gmail.com>
2025-09-23 11:13:08 -0700

    Create bootstrap form with client-side form validation

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,20 +1,35 @@
 (function () {
   "use strict";
 
+  // get the contact form
+  let form = document.querySelector("#contact-form");
+
   document
     .querySelector("#contact-form-button")
-    .addEventListener("click", (event) => {
+    .addEventListener("submit", (event) => {
       event.preventDefault();
       event.stopPropagation();
-      // log contact form button click
-      console.log("You clicked the submit button.");
+      let formValid = true;
+      if (!form.checkValidity()) {
+        formValid = false;
+      }
 
-      // get data from form
-      let name = document.querySelector("#name").value;
-      let email = document.querySelector("#mail").value;
-      let message = document.querySelector("#msg").value;
+      form.classList.add("was-validated");
 
-      // log data from form
-      console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+      if (formValid) {
+        sendTheEmail();
+      }
     });
+  function sendTheEmail() {
+    // get data from form
+    let firstName = document.querySelector("#first-name").value;
+    let lastName = document.querySelector("#last-name").value;
+    let email = document.querySelector("#mail").value;
+    let message = document.querySelector("#msg").value;
+
+    // log data from form
+    console.log(
+      `Name: ${firsName} ${lastName}, Email: ${email}, Message: ${message}`
+    );
+  }
 })();
